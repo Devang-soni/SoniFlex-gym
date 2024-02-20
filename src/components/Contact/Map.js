9e2bfdcc27abdb948c318586745cade527a50bc5
Devang  soni <127401975+Devang-soni@users.noreply.github.com>
2024-02-20 10:51:53 +0530

    Update Map.js

--- src/components/Contact/Map.js
+++ src/components/Contact/Map.js
@@ -4,9 +4,8 @@ import React from "react";
 
 const Map = () => (
   <div css={styles} className="mapouter">
-    <iframe
-      id="gmap_canvas"
-      src="https://maps.google.com/maps?q=Av.%20L%C3%BAcio%20Costa&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d375.4788654961346!2d72.84253368391795!3d19.185515541116306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b6fad76eee67%3A0xe4cedb89d25480f0!2sGajanan%20Niwas%20CHSL!5e1!3m2!1sen!2sin!4v1708405581905!5m2!1sen!2sin" 
+
     ></iframe>
   </div>
 );
